de55ed3ac883c90cdb3e8e46c70a35791b28ef56
yash-0000 <138404179+yash-0000@users.noreply.github.com>
2023-09-08 20:39:33 +0530

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,15 @@
+// 1 question simple interest
+let principal=Number(prompt("Enter principal amount"));
+let rate=Number(prompt("Enter rate of interest 1 to 10%"));
+let time=Number(prompt("for how many months enter 1-12"));
+
+alert((principal*rate*time)/100);
+
+// area of circle
+let radius=Number(prompt("enter radius you will get the area"));
+
+alert((3.14*radius*radius));
+
+// Volume of cube
+let edge=Number(prompt("enter edge you will get the area of cube"));
+alert((6*edge*edge));
